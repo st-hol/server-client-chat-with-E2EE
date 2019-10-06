20082247869c59bb600578c6a2979e1ce065840f
Stas <stanislav.holovachuk@gmail.com>
2019-10-06 13:20:24 +0300

    GUI added

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket, threading, time, re
+import tkinter
 
 import DH_Endpoint as dh
 import util
@@ -9,6 +10,7 @@ shutdown = False
 established = False
 join = False
 connected = False
+alias = ""
 
 opponent_public = 0
 opponent_partial = 0
@@ -16,22 +18,67 @@ dh_endpoint = 0
 my_partial = 0
 full_key = 0
 
-my_public = util.get_random_key()
-print("my pub = ", my_public)
-my_private = util.get_random_key()
-print("my prvt = ", my_private)
+
+def on_closing(event=None):
+    my_msg.set("{quit}")
+    send()
+
+
+def send(event=None):  # event binded
+    msg = my_msg.get()
+    my_msg.set("")  # Clear input
+    sock.sendto(("[" + alias + "]" + msg).encode("utf-8"), server)
+    msg_list.insert(tkinter.END, "[you] :" + msg)
+    if msg == "{quit}":
+        sock.close()
+        top.quit()
+
+
+top = tkinter.Tk('300x900')
+top.title("client-server chat with E2EE")
+
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()  # For the messages
+my_msg.set("")
+scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate
+#
+msg_list = tkinter.Listbox(messages_frame, height=15, background='black', fg='green', width=50,
+                           yscrollcommand=scrollbar.set, font=("Lucida Console", 14))
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+entry_field = tkinter.Entry(top, textvariable=my_msg, width=30, font=("Lucida Console", 10))
+entry_field.bind("<Return>", send)  # send entry if enter pressed
+entry_field.pack()
+send_button = tkinter.Button(top, text="ВІДПРАВИТИ", command=send, background='blue', fg='white', font=("Lucida Console", 14))
+send_button.pack()
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
+# alias = util.input_string_name()
+
+
+host = socket.gethostbyname(socket.gethostname())
+port = 0
+server = (host, 9090)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((host, port))
+sock.setblocking(0)
 
 
 def establishE2E(name, sock):
-    global established
-    global connected
-    global opponent_partial
-    global opponent_public
-    global dh_endpoint
-    global my_partial
-    global full_key
+    global established, connected
+    global opponent_partial, opponent_public, dh_endpoint, my_partial, full_key
+    global alias, my_public, my_private
 
     lock.acquire()
+
+    my_public = util.get_random_key()
+    print("my pub = ", my_public)
+    my_private = util.get_random_key()
+    print("my prvt = ", my_private)
+
     while not established:
         try:
             while True:
@@ -45,6 +92,12 @@ def establishE2E(name, sock):
                     print("opponent's PUBLIC key : ", opponent_public)
 
                     order = int(re.findall(r'#\d+#', data.decode("utf-8"))[0].strip("#")) == 0
+                    # print(int(re.findall(r'#\d+#', data.decode("utf-8"))[0].strip("#")))
+                    # if order == 0:
+                    #     alias = "first"
+                    # else:
+                    #     alias = "second"
+                    alias = "opponent"
                     # print("ord", re.findall(r'#\d+#', data.decode("utf-8"))[0].strip("#"),"  ")
                     dh_endpoint = dh.DH_Endpoint(my_public, opponent_public, my_private, order)
                     my_partial = dh_endpoint.generate_partial_key()
@@ -75,6 +128,7 @@ def establishE2E(name, sock):
                 time.sleep(0.2)
         except:
             pass
+    msg_list.insert(tkinter.END, "End-to-end connection established. \nYou can chat now!")
     lock.release()
 
 
@@ -109,6 +163,7 @@ def receiving(name, sock):
                     # \d# not found in the original string
                     found = ''
                 print(found)
+                msg_list.insert(tkinter.END, found)
                 # End
                 time.sleep(0.2)
         except:
@@ -116,50 +171,105 @@ def receiving(name, sock):
     lock.release()
 
 
-host = socket.gethostbyname(socket.gethostname())
-port = 0
-server = (host, 9090)
+def sending(name, sock):
+    global shutdown, join
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((host, port))
-sock.setblocking(0)
+    while not shutdown:
+        if not join:
+            sock.sendto(("[" + alias + "] => joined chat ").encode("utf-8"), server)
+            join = True
+        elif not connected:
+            sock.sendto(("[" + alias + "] => my user public_key is " + str(my_public)).encode("utf-8"), server)
+            # s.sendto(("[" + alias + "] => my user partial_key is " + str(my_partial)).encode("utf-8"), server)
+            time.sleep(0.2)
+        else:
+            try:
+                message = input(">\n")
+                # print('[you]: ' + message)
 
-alias = util.input_string_name()
+                # Begin
+                crypt = ""
+                for i in message:
+                    crypt += chr(ord(i) ^ full_key)
+                message = crypt
+                # End
+
+                if message != "":
+                    sock.sendto(("[" + alias + "] says: " + message).encode("utf-8"), server)
+
+                time.sleep(0.2)
+            except:
+                sock.sendto(("[" + alias + "] <= left chat ").encode("utf-8"), server)
+                shutdown = True
+
 
 establishSecureConnectionThread = threading.Thread(target=establishE2E, args=("E2EThread", sock))
 establishSecureConnectionThread.start()
 receivingThread = threading.Thread(target=receiving, args=("RecvThread", sock))
 receivingThread.start()
+sendingThread = threading.Thread(target=sending, args=("SendThread", sock))
+sendingThread.start()
 
-# messaging
-while not shutdown:
-    if not join:
-        sock.sendto(("[" + alias + "] => joined chat ").encode("utf-8"), server)
-        join = True
-    elif not connected:
-        sock.sendto(("[" + alias + "] => my user public_key is " + str(my_public)).encode("utf-8"), server)
-        # s.sendto(("[" + alias + "] => my user partial_key is " + str(my_partial)).encode("utf-8"), server)
-        time.sleep(0.2)
-    else:
-        try:
-            message = input(">\n")
-            # print('[you]: ' + message)
+# top.protocol("WM_DELETE_WINDOW", on_closing)
 
-            # Begin
-            crypt = ""
-            for i in message:
-                crypt += chr(ord(i) ^ full_key)
-            message = crypt
-            # End
 
-            if message != "":
-                sock.sendto(("[" + alias + "] says: " + message).encode("utf-8"), server)
-
-            time.sleep(0.2)
-        except:
-            sock.sendto(("[" + alias + "] <= left chat ").encode("utf-8"), server)
-            shutdown = True
-
+tkinter.mainloop()  # Starts GUI execution.
 establishSecureConnectionThread.join()
 receivingThread.join()
+sendingThread.join()
 sock.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### messaging
+#
+# def send():
+#     global shutdown, join
+#
+#     if not shutdown:
+#         if not join:
+#             sock.sendto(("[" + alias + "] => joined chat ").encode("utf-8"), server)
+#             join = True
+#         elif not connected:
+#             sock.sendto(("[" + alias + "] => my user public_key is " + str(my_public)).encode("utf-8"), server)
+#             # s.sendto(("[" + alias + "] => my user partial_key is " + str(my_partial)).encode("utf-8"), server)
+#             time.sleep(0.2)
+#         else:
+#             try:
+#                 message = input(">\n")
+#                 # print('[you]: ' + message)
+#
+#                 # Begin
+#                 crypt = ""
+#                 for i in message:
+#                     crypt += chr(ord(i) ^ full_key)
+#                 message = crypt
+#                 # End
+#
+#                 if message != "":
+#                     sock.sendto(("[" + alias + "] says: " + message).encode("utf-8"), server)
+#
+#                 time.sleep(0.2)
+#             except:
+#                 sock.sendto(("[" + alias + "] <= left chat ").encode("utf-8"), server)
+#                 shutdown = True
+#
